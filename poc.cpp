755acbad54d23a8c999a066b673c54875ed60f9c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-02-12 21:46:47 +0000

    moving code around

--- poc.cpp
+++ poc.cpp
@@ -139,6 +139,69 @@ public:
   using update_thread::run_once;
 };
 
+[[nodiscard]] static auto draw_example(instances *is) {
+  auto p = is->pen();
+
+  p.aperture(0.1); // D10C,0.1
+  p.move(0, 2.5);
+  p.draw(0, 0);
+  p.draw(2.5, 0);
+  p.move(10, 10);
+  p.draw_x(15);
+  p.draw(20, 15);
+  p.move_x(25);
+  p.draw_y(10);
+
+  p.aperture(0.6); // D11C,0.6
+  p.flash(10, 10);
+  p.flash_x(20);
+  p.flash_x(25);
+  p.flash_y(15);
+  p.flash_x(20);
+
+  // we simulate rectangles/obround with a small segment
+
+  // TODO: rectangle aperture
+  p.aperture(0.6, 0.6, false); // D12R,0.6x0.6
+  p.flash(10, 15);
+
+  p.aperture(0.4, 1.0, false); // D13R,0.4x1.00
+  p.flash(30, 15);
+
+  p.aperture(1.0, 0.4, false); // D14R,1.00X0.4
+  p.flash_y(12.5);
+
+  p.aperture(0.4, 1.0, true); // D15O0.4X1.00
+  p.flash_y(10);
+
+  p.aperture(0.1); // D10C,0.1
+  p.move(37.5, 10);
+  // p.arc(37.5, 10, 25.0, 0);
+
+  p.aperture(1);
+  // p.aperture(1, 3); // D16P,1.00X3 "triangle"
+  p.flash(34, 10);
+  p.flash(35, 9);
+
+  // TODO: add support for regions
+  // Most probably, take note of the count here and draw lines over two
+  // calls and regions as a different pipeline?
+
+  // TODO: "clear" polarity
+
+  p.aperture(0.1); // D10
+  p.move(15, 28.75);
+  p.draw_x(20);
+
+  p.aperture(0.6); // D11
+  p.flash(15, 28.75);
+  p.flash_x(20);
+
+  // TODO: "thermal" macro
+
+  return p.count();
+}
+
 class thread : public voo::casein_thread {
 public:
   void run() override {
@@ -154,69 +217,7 @@ public:
     while (!interrupted()) {
       voo::swapchain_and_stuff sw{dq};
 
-      unsigned i_count{};
-      {
-        auto p = is.pen();
-
-        p.aperture(0.1); // D10C,0.1
-        p.move(0, 2.5);
-        p.draw(0, 0);
-        p.draw(2.5, 0);
-        p.move(10, 10);
-        p.draw_x(15);
-        p.draw(20, 15);
-        p.move_x(25);
-        p.draw_y(10);
-
-        p.aperture(0.6); // D11C,0.6
-        p.flash(10, 10);
-        p.flash_x(20);
-        p.flash_x(25);
-        p.flash_y(15);
-        p.flash_x(20);
-
-        // we simulate rectangles/obround with a small segment
-
-        // TODO: rectangle aperture
-        p.aperture(0.6, 0.6, false); // D12R,0.6x0.6
-        p.flash(10, 15);
-
-        p.aperture(0.4, 1.0, false); // D13R,0.4x1.00
-        p.flash(30, 15);
-
-        p.aperture(1.0, 0.4, false); // D14R,1.00X0.4
-        p.flash_y(12.5);
-
-        p.aperture(0.4, 1.0, true); // D15O0.4X1.00
-        p.flash_y(10);
-
-        p.aperture(0.1); // D10C,0.1
-        p.move(37.5, 10);
-        // p.arc(37.5, 10, 25.0, 0);
-
-        p.aperture(1);
-        // p.aperture(1, 3); // D16P,1.00X3 "triangle"
-        p.flash(34, 10);
-        p.flash(35, 9);
-
-        // TODO: add support for regions
-        // Most probably, take note of the count here and draw lines over two
-        // calls and regions as a different pipeline?
-
-        // TODO: "clear" polarity
-
-        p.aperture(0.1); // D10
-        p.move(15, 28.75);
-        p.draw_x(20);
-
-        p.aperture(0.6); // D11
-        p.flash(15, 28.75);
-        p.flash_x(20);
-
-        // TODO: "thermal" macro
-
-        i_count = p.count();
-      }
+      unsigned i_count = draw_example(&is);
       is.run_once();
 
       auto gp = vee::create_graphics_pipeline({
