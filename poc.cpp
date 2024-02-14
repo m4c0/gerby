22206cdc85f91dabcc20c36977d86c0a7651a3fd
Eduardo Costa <m4c0@users.noreply.github.com>
2024-02-14 19:22:41 +0000

    centralising the 555

--- poc.cpp
+++ poc.cpp
@@ -98,6 +98,10 @@ public:
 
   [[nodiscard]] constexpr auto value() const noexcept { return m_val; }
 
+  [[nodiscard]] constexpr auto operator-() const noexcept {
+    return distance<Inches>{-m_val};
+  }
+
   [[nodiscard]] explicit constexpr operator distance<!Inches>() const noexcept;
 };
 
@@ -139,18 +143,32 @@ constexpr auto operator*(const distance<I> &a, long double n) {
 // https://www.pcb-3d.com/tutorials/how-to-calculate-pth-hole-and-pad-diameter-sizes-according-to-ipc-7251-ipc-2222-and-ipc-2221-standards/
 constexpr const auto pdip_pin_allowance = 0.1_mm;
 constexpr const auto pdip_pin_diam_max = 0.021_in;
+constexpr const auto pdip_pin_dist = 0.1_in;
 constexpr const auto pdip_pin_hole = pdip_pin_diam_max + 0.2_mm;
 constexpr const auto pdip_pin_pad = pdip_pin_hole + pdip_pin_allowance + 0.5_mm;
 constexpr const auto pdip_width = 0.3_in;
-constexpr const auto pdip_draw_x = pdip_pin_pad;
-constexpr const auto pdip_draw_w = pdip_width - pdip_pin_pad;
+constexpr const auto pdip_draw_w = 0.25_in;
+constexpr const auto pdip_draw_cx = pdip_width * 0.5;
+constexpr const auto pdip_draw_h = 0.4_in;
+constexpr const auto pdip_draw_cy = -pdip_pin_dist * 1.5;
 
 constexpr const auto pdip = [](auto &p) {
   for (auto i = 0; i < 4; i++) {
-    auto x = 0.1_in * i;
-    p.flash(0, x.value());
-    p.flash(pdip_width.value(), x.value());
+    auto n = pdip_pin_dist * -i;
+    p.flash(0, n.value());
+    p.flash(pdip_width.value(), n.value());
   }
+};
+constexpr const auto rect = [](auto &p, auto cx, auto cy, auto w, auto h) {
+  auto l = (cx - w * 0.5).value();
+  auto t = (cy - h * 0.5).value();
+  auto r = (cx + w * 0.5).value();
+  auto b = (cy + h * 0.5).value();
+  p.move(l, t);
+  p.draw_y(b);
+  p.draw_x(r);
+  p.draw_y(t);
+  p.draw_x(l);
 };
 auto &pcb_example() {
   static gerby::thread t{[](auto b) {
@@ -169,11 +187,7 @@ auto &pcb_example() {
     b->add_lines(
         [](auto &p) {
           p.aperture(0.01);
-          p.move(pdip_draw_x.value(), 0);
-          p.draw_x(pdip_draw_w.value());
-          p.draw_y((0.1_in * 3).value());
-          p.draw_x(pdip_draw_x.value());
-          p.draw_y(0);
+          rect(p, pdip_draw_cx, pdip_draw_cy, pdip_draw_w, pdip_draw_h);
         },
         white);
   }};
