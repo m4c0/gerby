f94cc575cbe23b4704c84854d4781dcd96eb0c0f
Eduardo Costa <m4c0@users.noreply.github.com>
2024-02-16 13:08:53 +0000

    trying a component pattern

--- poc.cpp
+++ poc.cpp
@@ -5,61 +5,71 @@ import gerby;
 
 using namespace gerby::literals;
 
-// https://datasheet.lcsc.com/lcsc/2205311800_UNI-ROYAL-Uniroyal-Elec-0805W8F1001T5E_C17513.pdf
-// https://datasheet.lcsc.com/lcsc/2304140030_FH--Guangdong-Fenghua-Advanced-Tech-0805B471K500NT_C1743.pdf
-class r0805 {
+class compo {
+  gerby::d::inch m_x;
+  gerby::d::inch m_y;
+
+protected:
+  [[nodiscard]] constexpr auto x() const noexcept { return m_x; }
+  [[nodiscard]] constexpr auto y() const noexcept { return m_y; }
+
+public:
+  constexpr compo(gerby::d::inch x, gerby::d::inch y) : m_x{x}, m_y{y} {}
+
+  virtual void copper(gerby::pen &) const = 0;
+};
+
+class r0805 : public compo {
   static constexpr const auto a = 1.0_mm;
   static constexpr const auto b = 1.0_mm;
   static constexpr const auto c = 1.3_mm;
   static constexpr const auto d = 3.0_mm;
 
-  gerby::d::inch m_x;
-  gerby::d::inch m_y;
-
   void pads(auto &p) const {
     p.aperture(b, c, false);
-    p.flash(m_x, m_y);
-    p.flash_x(m_x + d - b);
+    p.flash(x(), y());
+    p.flash_x(x() + d - b);
   }
 
 public:
-  constexpr r0805(gerby::d::inch x, gerby::d::inch y) : m_x{x}, m_y{y} {}
+  using compo::compo;
 
-  void copper(auto &p) const { pads(p); }
+  void copper(gerby::pen &p) const override { pads(p); }
 };
 
-// https://www.ti.com/lit/ds/symlink/lm555.pdf
-class soic_8 {
+class soic_8 : public compo {
   static constexpr const auto pw = 1.55_mm;
   static constexpr const auto ph = 0.60_mm;
   static constexpr const auto dx = 5.40_mm;
   static constexpr const auto dy = 1.27_mm;
 
-  gerby::d::inch m_x;
-  gerby::d::inch m_y;
-
   void pads(auto &p) const {
     p.aperture(pw, ph, false);
     for (auto i = 0; i < 4; i++) {
-      p.flash(m_x, m_y - dy * i);
-      p.flash_x(m_x + dx);
+      p.flash(x(), y() - dy * i);
+      p.flash_x(x() + dx);
     }
   }
 
 public:
-  constexpr soic_8(gerby::d::inch x, gerby::d::inch y) : m_x{x}, m_y{y} {}
+  using compo::compo;
 
-  void copper(auto &p) const { pads(p); }
+  void copper(gerby::pen &p) const override { pads(p); }
 };
 
 extern "C" void casein_handle(const casein::event &e) {
   using namespace gerby::palette;
 
+  // https://datasheet.lcsc.com/lcsc/2205311800_UNI-ROYAL-Uniroyal-Elec-0805W8F1001T5E_C17513.pdf
+  // https://datasheet.lcsc.com/lcsc/2304140030_FH--Guangdong-Fenghua-Advanced-Tech-0805B471K500NT_C1743.pdf
+  // https://datasheet.lcsc.com/lcsc/1806151129_Hubei-KENTO-Elec-KT-0805Y_C2296.pdf
+  // https://www.ti.com/lit/ds/symlink/lm555.pdf
   static constexpr const r0805 r1{0, 0};
   static constexpr const r0805 r2{0, 2.0_mm};
   static constexpr const r0805 r3{0, 4.0_mm};
   static constexpr const r0805 c1{7.0_mm, 1.0_mm};
   static constexpr const r0805 c2{7.0_mm, 3.0_mm};
+  static constexpr const r0805 l1{7.0_mm, 6.0_mm};
   static constexpr const soic_8 ne555{-10.0_mm, 0};
 
   static gerby::thread t{[](auto b) {
@@ -70,6 +80,7 @@ extern "C" void casein_handle(const casein::event &e) {
           r3.copper(p);
           c1.copper(p);
           c2.copper(p);
+          l1.copper(p);
           ne555.copper(p);
         },
         red);
