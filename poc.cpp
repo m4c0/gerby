f021abb29dee8b37357a794e8898e9fe5d4fd69c
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-22 21:08:34 +0100

    poc: adding all of them

--- poc.cpp
+++ poc.cpp
@@ -1,6 +1,11 @@
 #pragma leco app
+#pragma leco add_plugin "poc-555-smd.cpp"
+#pragma leco add_plugin "poc-555-tht.cpp"
+#pragma leco add_plugin "poc-counter.cpp"
+#pragma leco add_plugin "poc-skeet.cpp"
 #pragma leco add_plugin "poc-tp4056.cpp"
+#pragma leco add_plugin "poc-ucamco.cpp"
 import casein;
 import gerbyv;
 
-static gerby::thread t{"poc-tp4056"};
+static gerby::thread t{"poc-counter"};
